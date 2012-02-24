b7f691872d939aec269fdac9964f3558f7af044e
Mikhail Korobov <kmike84@gmail.com>
2012-02-24 20:16:26 +0600

    tox tests; python 2.5 no longer listed as supported because WebOb doesn't support python 2.5.

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries :: Python Modules',

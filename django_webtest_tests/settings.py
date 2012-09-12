b229551bff455339ead4a3579e36252057c0e4cd
Mikhail Korobov <kmike84@gmail.com>
2012-09-12 22:33:36 +0600

    update tox config with recent django versions

--- django_webtest_tests/settings.py
+++ django_webtest_tests/settings.py
@@ -61,7 +61,7 @@ MEDIA_URL = '/media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/admin/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '5mcs97ar-(nnxhfkx0%^+0^sr!e(ax=x$2-!8dqy25ff-l1*a='

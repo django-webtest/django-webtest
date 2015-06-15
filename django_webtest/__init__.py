b227ee19f4457b851cc4a97cef311676a2618453
Julien Phalip <jphalip@gmail.com>
2015-06-15 00:08:16 -0700

    Get around webtest storing the cookie value with surrounding quotes

--- django_webtest/__init__.py
+++ django_webtest/__init__.py
@@ -164,6 +164,7 @@ class DjangoTestApp(TestApp):
             engine = import_module(settings.SESSION_ENGINE)
             cookie = self.cookies.get(settings.SESSION_COOKIE_NAME, None)
             if cookie:
+                cookie = cookie[1:-1]  # Remove surrounding quotes
                 return engine.SessionStore(cookie)
             else:
                 session = engine.SessionStore()

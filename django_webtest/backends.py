9f867f832183bae855764965ba37aa7cf4d61d24
Peter Inglesby <peter.inglesby@gmail.com>
2017-01-19 00:04:37 +0000

    Update signature of WebtestUserBackend.authenticate

--- django_webtest/backends.py
+++ django_webtest/backends.py
@@ -6,9 +6,9 @@ from .compat import from_wsgi_safe_string
 class WebtestUserBackend(RemoteUserBackend):
     """ Auth backend for django-webtest auth system """
 
-    def authenticate(self, django_webtest_user):
+    def authenticate(self, request, django_webtest_user):
         return super(WebtestUserBackend, self).authenticate(
-            django_webtest_user)
+            request, django_webtest_user)
 
     def clean_username(self, username):
         return from_wsgi_safe_string(username)

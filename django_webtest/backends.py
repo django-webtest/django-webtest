75dca1c2d39126556ded5f328461986a9eabb230
Peter Inglesby <peter.inglesby@gmail.com>
2017-01-19 13:54:18 +0000

    Define WebtestUserBackend.authenticate based on Django version
    This is required because the signature of RemoteUserBackend.authenticate
    has changed in Django 1.11.

--- django_webtest/backends.py
+++ django_webtest/backends.py
@@ -1,14 +1,19 @@
 from __future__ import absolute_import
+from django.utils.version import get_complete_version
 from django.contrib.auth.backends import RemoteUserBackend
 from .compat import from_wsgi_safe_string
-
 
 class WebtestUserBackend(RemoteUserBackend):
     """ Auth backend for django-webtest auth system """
 
-    def authenticate(self, request, django_webtest_user):
-        return super(WebtestUserBackend, self).authenticate(
-            request, django_webtest_user)
+    if get_complete_version() >= (1, 11):
+        def authenticate(self, request, django_webtest_user):
+            return super(WebtestUserBackend, self).authenticate(
+                request, django_webtest_user)
+    else:
+        def authenticate(self, django_webtest_user):
+            return super(WebtestUserBackend, self).authenticate(
+                django_webtest_user)
 
     def clean_username(self, username):
         return from_wsgi_safe_string(username)

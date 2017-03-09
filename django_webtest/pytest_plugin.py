2cd30ce3782641981260f90110bebb72a3c26d14
Gael Pasgrimaud <gael@gawel.org>
2017-03-09 18:25:52 +0100

    do not use dict as default arg; code reorg

--- django_webtest/pytest_plugin.py
+++ django_webtest/pytest_plugin.py
@@ -10,23 +10,24 @@ def django_app_mixin():
 
 
 @pytest.yield_fixture
+def django_app(django_app_mixin):
+    django_app_mixin._patch_settings()
+    django_app_mixin.renew_app()
+    yield django_app_mixin.app
+    django_app_mixin._unpatch_settings()
+
+
+@pytest.yield_fixture
 def django_app_factory():
     app_mixin = WebTestMixin()
 
-    def factory(csrf_checks=True, extra_environ={}):
+    def factory(csrf_checks=True, extra_environ=None):
         app_mixin.csrf_checks = csrf_checks
-        app_mixin.extra_environ = extra_environ
+        if extra_environ:
+            app_mixin.extra_environ = extra_environ
         app_mixin._patch_settings()
         app_mixin.renew_app()
         return app_mixin.app
 
     yield factory
     app_mixin._unpatch_settings()
-
-
-@pytest.yield_fixture
-def django_app(django_app_mixin):
-    django_app_mixin._patch_settings()
-    django_app_mixin.renew_app()
-    yield django_app_mixin.app
-    django_app_mixin._unpatch_settings()

2a8b5cf9b68a1aef2666da3895b97fd57e4e0e4e
Jayram Nai <jramnai007@gmail.com>
2021-09-07 20:26:22 +0530

    feat: Tested against Django 3.1 and 3.2

--- setup.py
+++ setup.py
@@ -53,6 +53,8 @@ setup(
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',

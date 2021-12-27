01d1d59dc228ef0496380c68668cbcefc5f3d8a0
Gael Pasgrimaud <gael@gawel.org>
2021-12-27 10:27:23 +0100

    django 4.0 support

--- django_webtest_tests/urls.py
+++ django_webtest_tests/urls.py
@@ -1,13 +1,21 @@
-try:
-    from django.conf.urls import url
-except ImportError:
-    from django.conf.urls.defaults import url
-
+import django
 from django.http import HttpResponse
 from django.shortcuts import render
 
 from testapp_tests.views import check_password, search, set_session, \
     protected, redirect_to_protected, remove_prefix_redirect, cookie_test
+
+
+major_version = int(django.__version__[0])
+
+if major_version >= 4:
+    from django.urls import re_path
+else:
+    try:
+        from django.conf.urls import url
+    except ImportError:
+        from django.conf.urls.defaults import url
+
 
 try:
     from django.contrib.auth.views import login
@@ -30,18 +38,35 @@ def simple_template_render(request, template_name):
     })
 
 
-urlpatterns = (
-    url(r'^$', simple_method_test, name='simple-method-test'),
-    url(r'^template/(.*)$', simple_template_render,
-        name='simple-template-test'),
-    url(r'^check-password/$', check_password, name='check_password'),
-    url(r'^search/$', search, name='search'),
-    url(r'^login/$', login, name='auth_login'),
-    url(r'^set-session/$', set_session, name='set_session'),
-    url(r'^protected/$', protected, name='protected'),
-    url(r'^redirect-to-protected/$',
-        redirect_to_protected, name='redirect-to-protected'),
-    url(r'^remove-prefix-redirect/(.*)/$',
-        remove_prefix_redirect, name='remove-prefix-redirect'),
-    url(r'^cookie-test/$', cookie_test, name='cookie_test'),
-)
+if major_version >= 4:
+    urlpatterns = (
+        re_path(r'$', simple_method_test, name='simple-method-test'),
+        re_path(r'template/(.*)$', simple_template_render,
+                name='simple-template-test'),
+        re_path(r'check-password/$', check_password, name='check_password'),
+        re_path(r'search/$', search, name='search'),
+        re_path(r'login/$', login, name='auth_login'),
+        re_path(r'set-session/$', set_session, name='set_session'),
+        re_path(r'protected/$', protected, name='protected'),
+        re_path(r'redirect-to-protected/$',
+                redirect_to_protected, name='redirect-to-protected'),
+        re_path(r'remove-prefix-redirect/(.*)/$',
+                remove_prefix_redirect, name='remove-prefix-redirect'),
+        re_path(r'cookie-test/$', cookie_test, name='cookie_test'),
+    )
+else:
+    urlpatterns = (
+        url(r'^$', simple_method_test, name='simple-method-test'),
+        url(r'^template/(.*)$', simple_template_render,
+            name='simple-template-test'),
+        url(r'^check-password/$', check_password, name='check_password'),
+        url(r'^search/$', search, name='search'),
+        url(r'^login/$', login, name='auth_login'),
+        url(r'^set-session/$', set_session, name='set_session'),
+        url(r'^protected/$', protected, name='protected'),
+        url(r'^redirect-to-protected/$',
+            redirect_to_protected, name='redirect-to-protected'),
+        url(r'^remove-prefix-redirect/(.*)/$',
+            remove_prefix_redirect, name='remove-prefix-redirect'),
+        url(r'^cookie-test/$', cookie_test, name='cookie_test'),
+    )

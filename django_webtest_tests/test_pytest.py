aa5a46033ce7785791bcc0ecffefe1df2de8969d
Jannis Vajen <jannis.vajen@kraken.tech>
2024-08-27 14:50:03 +0200

    Add test for unpatching settings
    django_app_factory should unpatch the modified Django settings,
    just as django_app does.

--- django_webtest_tests/test_pytest.py
+++ django_webtest_tests/test_pytest.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import pytest
+from django.conf import settings
+
+initial_settings = {
+    "DEBUG_PROPAGATE_EXCEPTIONS": settings.DEBUG_PROPAGATE_EXCEPTIONS,
+    "AUTHENTICATION_BACKENDS": settings.AUTHENTICATION_BACKENDS,
+}
 
 def test_django_app(django_app):
     resp = django_app.get('/')
@@ -10,3 +17,9 @@ def test_django_app_post(django_app_factory):
     app = django_app_factory(csrf_checks=False)
     resp = app.post('/')
     assert resp.status_int == 200
+
+@pytest.mark.xfail(reason="django_app_factory does not unpatch settings")
+def test_app_factory():
+    """Ensure django_app_factory properly resets settings."""
+    assert settings.DEBUG_PROPAGATE_EXCEPTIONS is initial_settings["DEBUG_PROPAGATE_EXCEPTIONS"]
+    assert settings.AUTHENTICATION_BACKENDS == initial_settings["AUTHENTICATION_BACKENDS"]
